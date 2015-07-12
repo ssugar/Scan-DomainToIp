1e97e4a63a263a78232607aa43a507db15e42a56
ssugar <scottsugar@outlook.com>
2015-07-12 12:14:42 +0700

    initial commit

---
+++ Get-DomainIpRange.ps1
@@ -0,0 +1,18 @@
+﻿param(
+  $dns,
+  $onlyTrueFlag=0
+)
+$file = Get-Content .\hosts.txt
+$lines = $file.Split([Environment]::NewLine)
+foreach($line in $lines)
+{
+    $hostname = $line + "." + $dns
+    Try
+    {
+        [System.Net.Dns]::GetHostAddresses($hostname) | foreach {write-host $hostname "resolves to" $_.IPAddressToString }
+    }
+    Catch
+    {
+        if($onlyTruFlag -eq 0){write-host $hostname "not found"}
+    }
+}
