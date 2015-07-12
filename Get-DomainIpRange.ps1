f39df569c90066d68348348c9b43db3e6e2eb40b
ssugar <scottsugar@outlook.com>
2015-07-12 14:00:06 +0700

    adding ability to pull mx, txt and spf records

--- Get-DomainIpRange.ps1
+++ Get-DomainIpRange.ps1
@@ -1,8 +1,15 @@
 ﻿param(
   $dns,
+  [parameter()]
+  [ValidateScript({
+    If(Test-Path $_){$true}else{Throw "Invalid wordlist file given: $_"}
+  })]
+  [string[]]$wordlist,
   $onlyTrueFlag=0
 )
-$file = Get-Content .\hosts.txt
+
+
+$file = Get-Content $wordlist
 $lines = $file.Split([Environment]::NewLine)
 foreach($line in $lines)
 {
@@ -15,4 +22,41 @@ foreach($line in $lines)
     {
         if($onlyTruFlag -eq 0){write-host $hostname "not found"}
     }
+}
+
+$mxRecords = Nslookup -type=mx $dns 2> $null
+$x=0
+foreach($item in $mxRecords)
+{
+    if($x -gt 2) #skip over the server, address and blank line output from nslookup
+    {
+        write-host $item #write out just the mail exchanger lines
+    }
+    $x+=1
+}
+
+$txtRecords = Nslookup -type=txt $dns 2> $null
+$x=0
+foreach($item in $txtRecords)
+{
+    if($x -gt 2) #skip over the server, address and blank line output from nslookup
+    {
+        write-host $item #write out just the txt record lines
+        if($item -match "v=spf1")
+        {
+            $segments = $item.Split(":")
+            $targetSegment = ($segments[1].Split(" "))[0]
+            $spfIps = Nslookup -type=txt $targetSegment 2> $null
+            $y=0
+            foreach($spfIp in $spfIps)
+            {
+                if($y -gt 2) #skip over the server, address and blank line output from nslookup
+                {
+                    write-host $spfIp
+                }
+                $y+=1
+            }
+        }
+    }
+    $x+=1
 }
